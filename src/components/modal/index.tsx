534186ca2ae8298812cce3d66b83db667d2b7266
Yasin ERKAN <yasinerkan@gmail.com>
2025-03-11 22:39:00 +0100

    Update index.tsx

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,17 +1,15 @@
-import {FC} from 'react';
-import {Car} from '../../types';
-import Images from './images';
-import formatData from '../../utils/formatdData';
+import { FC } from "react";
+import { Car } from "../../types";
+import Images from "./images";
+import formatData from "../../utils/formatData";
 
 interface Props {
   isOpen: boolean;
-  key: string;
-  children: JSX.Element;
   close: () => void;
   car: Car;
 }
 
-const Modal: FC<Props> = ({isOpen, close, car, children}) => {
+const Modal: FC<Props> = ({ isOpen, close, car }) => {
   return (
     isOpen && (
       <div className="fixed inset-0 bg-black/50 backdrop-blur-[2px] grid place-items-center z-20">
@@ -19,7 +17,7 @@ const Modal: FC<Props> = ({isOpen, close, car, children}) => {
           <button
             onClick={close}
             className="cursor-pointer p-1 absolute end-1 top-1 z-10 bg-white rounded-full">
-            <img src="/public/close.svg" alt="close" />
+            <img src="/close.svg" alt="close" />
           </button>
 
           {/* photos */}
@@ -30,11 +28,7 @@ const Modal: FC<Props> = ({isOpen, close, car, children}) => {
             <p key={key} className="flex justify-between gap-20">
               <span className="capitalize">{key}</span>
               <span className="font-semibold capitalize">
-                {value === 'Y'
-                  ? 'Yes'
-                  : value === 'N'
-                  ? 'Non-exist'
-                  : value || '-'}
+                {value === "Y" ? "Yes" : value === "N" ? "Non-exist" : value || "-"}
               </span>
             </p>
           ))}
