56c25ca625b449c523f06bc5bb546d6d8d145c32
Yasin ERKAN <yasinerkan@gmail.com>
2025-03-11 21:58:52 +0100

    Update index.tsx

--- src/components/warning/index.tsx
+++ src/components/warning/index.tsx
@@ -1,14 +1,15 @@
-import React, { FC } from 'react'
+import { FC, JSX } from "react";
 
 interface Props {
   children: JSX.Element | string;
 }
-const Warning:FC<Props> = ({children}) => {
-  return (
-    <div className='padding-x max-width'>
-      <div className='home-error-container'>{children}</div>
-    </div>
-  )
-}
 
-export default Warning
+const Warning: FC<Props> = ({ children }) => {
+  return (
+    <div className="padding-x max-width">
+      <div className="home-error-container">{children}</div>
+    </div>
+  );
+};
+
+export default Warning;
