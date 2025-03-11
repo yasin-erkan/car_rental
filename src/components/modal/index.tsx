5a7c38e2d04bbdc3a68f8ce661fa1b089d64c32c
Yasin ERKAN <yasinerkan@gmail.com>
2025-03-11 22:38:03 +0100

    Update index.tsx

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,20 +1,24 @@
-import { FC } from "react";
-import { Car } from "../../types";
-import Images from "./images";
-import formatData from "../../utils/formatData";
+import {FC} from 'react';
+import {Car} from '../../types';
+import Images from './images';
+import formatData from '../../utils/formatdData';
 
 interface Props {
   isOpen: boolean;
+  key: string;
+  children: JSX.Element;
   close: () => void;
   car: Car;
 }
 
-const Modal: FC<Props> = ({ isOpen, close, car, children }) => {
+const Modal: FC<Props> = ({isOpen, close, car, children}) => {
   return (
     isOpen && (
       <div className="fixed inset-0 bg-black/50 backdrop-blur-[2px] grid place-items-center z-20">
         <div className="bg-white p-6 relative max-w-lg max-h-[90vh] rounded-2xl flex flex-col gap-5 shadow-xl overflow-auto">
-          <button onClick={close} className="cursor-pointer p-1 absolute end-1 top-1 z-10 bg-white rounded-full">
+          <button
+            onClick={close}
+            className="cursor-pointer p-1 absolute end-1 top-1 z-10 bg-white rounded-full">
             <img src="/public/close.svg" alt="close" />
           </button>
 
@@ -23,10 +27,14 @@ const Modal: FC<Props> = ({ isOpen, close, car, children }) => {
 
           {/* infos */}
           {formatData(car).map(([key, value]) => (
-            <p className="flex justify-between gap-20">
+            <p key={key} className="flex justify-between gap-20">
               <span className="capitalize">{key}</span>
               <span className="font-semibold capitalize">
-                {value === "Y" ? "Yes" : value === "N" ? "Non-exist" : value || "-"}
+                {value === 'Y'
+                  ? 'Yes'
+                  : value === 'N'
+                  ? 'Non-exist'
+                  : value || '-'}
               </span>
             </p>
           ))}
