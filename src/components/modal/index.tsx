53523579aef95890ebf84acb400e184c62f3af39
Yasin ERKAN <yasinerkan@gmail.com>
2025-03-11 22:41:50 +0100

    Update index.tsx

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import { Car } from "../../types";
 import Images from "./images";
-import formatData from "../../utils/formatData";
+import formatData from "../../utils/formatdData";
 
 interface Props {
   isOpen: boolean;
@@ -14,21 +14,19 @@ const Modal: FC<Props> = ({ isOpen, close, car }) => {
     isOpen && (
       <div className="fixed inset-0 bg-black/50 backdrop-blur-[2px] grid place-items-center z-20">
         <div className="bg-white p-6 relative max-w-lg max-h-[90vh] rounded-2xl flex flex-col gap-5 shadow-xl overflow-auto">
-          <button
-            onClick={close}
-            className="cursor-pointer p-1 absolute end-1 top-1 z-10 bg-white rounded-full">
+          <button onClick={close} className="cursor-pointer p-1 absolute end-1 top-1 z-10 bg-white rounded-full">
             <img src="/close.svg" alt="close" />
           </button>
 
-          {/* photos */}
+          {/* fotoğraflar */}
           <Images car={car} />
 
-          {/* infos */}
+          {/* bilgiler */}
           {formatData(car).map(([key, value]) => (
-            <p key={key} className="flex justify-between gap-20">
+            <p className="flex justify-between gap-20">
               <span className="capitalize">{key}</span>
               <span className="font-semibold capitalize">
-                {value === "Y" ? "Yes" : value === "N" ? "Non-exist" : value || "-"}
+                {value === "Y" ? "Var" : value === "N" ? "Yok" : value || "-"}
               </span>
             </p>
           ))}
