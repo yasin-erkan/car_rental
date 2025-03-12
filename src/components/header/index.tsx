00feb1b0b51a69dd8f4aa6aee1b074bab280698a
Yasin Erkan <yasinerkan@gmail.com>
2025-03-12 21:40:06 +0100

    gif updated

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,18 +1,18 @@
-import React, { FC } from 'react'
-import { Link } from 'react-router-dom'
-import Button from '../button'
+import React, {FC} from 'react';
+import {Link} from 'react-router-dom';
+import Button from '../button';
 
-const Header:FC = () => {
+const Header: FC = () => {
   return (
     <header w-full z-10>
-      <div className='max-with flex justify-between items-center padding-x py-4'>
-        <Link to= "/" >
-        <img src="/public/bmw.png" alt="bmw logo" width={50} height={50} />
+      <div className="max-with flex justify-between items-center padding-x py-4">
+        <Link to="/">
+          <img src="/public/bmw.png" alt="bmw logo" width={50} height={50} />
         </Link>
-        <Button text='Subscribe' designs='min-w-[130px]'/>
+        <Button text="Subscribe" designs="min-w-[130px]" />
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header 
+export default Header;
